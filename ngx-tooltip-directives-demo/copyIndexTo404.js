5da9f1fc2d9ed1b8d8e91c1b77b0ffe7e0ec1f78
Matthias Keller <matthias.b.keller@bluewin.ch>
2024-05-25 23:22:15 +0200

    Upgrade to Angular 18

--- ngx-tooltip-directives-demo/copyIndexTo404.js
+++ ngx-tooltip-directives-demo/copyIndexTo404.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const srcPath = path.join(__dirname, 'dist', 'ngx-tooltip-directives-demo', 'index.html');
-const destPath = path.join(__dirname, 'dist', 'ngx-tooltip-directives-demo', '404.html');
+const srcPath = path.join(__dirname, 'dist', 'ngx-tooltip-directives-demo', 'browser', 'index.html');
+const destPath = path.join(__dirname, 'dist', 'ngx-tooltip-directives-demo','browser', '404.html');
 
 fs.copyFile(srcPath, destPath, (err) => {
   if (err) throw err;
