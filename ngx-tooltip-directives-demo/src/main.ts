6b5e2dd86d96aba5f79a4318e0cd8c4608c2f4e0
Matthias Keller <matthias.b.keller@bluewin.ch>
2025-08-22 17:32:02 +0200

    Demo-app: Removed obsolete "provideAnimations()"

--- ngx-tooltip-directives-demo/src/main.ts
+++ ngx-tooltip-directives-demo/src/main.ts
@@ -5,15 +5,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 bootstrapApplication(AppComponent,
   {
     providers: [
-      provideRouter(APP_ROUTES, withComponentInputBinding()),
-      provideAnimations(),
-      provideZonelessChangeDetection(),
+		provideRouter(APP_ROUTES, withComponentInputBinding()),
+		provideZonelessChangeDetection(),
     ]
   })
   .catch(err => console.error(err));
